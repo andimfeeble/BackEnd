8358e85f5c81b822de739f61885f4cf38e6c375f
andimfeeble <andimfeeble@hotmail.com>
2021-11-15 16:26:23 -0300

    15-11-21

--- Python/Teste.py
+++ Python/Teste.py
@@ -1,40 +1,49 @@
-#Exercicio89
-opcao = ''
-lista = []
-print('-------CADASTRO DE ALUNO--------')
-while opcao != 'N':
-    nome = str(input('Digite o nome do aluno: ')).strip().capitalize()
-    nota1 = float(input('Digite a primeira nota: '))
-    nota2 = float(input('Digite a segunda nota: '))
-    media = (nota1 + nota2) / 2
-    lista.append([nome, nota1, nota2, media])
-    print('-------Cadastro Realizado-------')
-    print()
-    opcao = str(input('Deseja cadastrar outro aluno? [S/N] ')).strip().upper()
-print()
-print('--------BOLETIM DA TURMA---------')
-print(f'{"Cod.":<5} {"NOME":<10} {"MEDIA":>10}')
-print('-' * 32)
-for indice, aluno in enumerate(lista):
-    print(f'{indice + 1:<5} {aluno[0]:<10} {aluno[3]:>10.1f}')
-opcao = ''
-print('-' * 32)
-print()
-while opcao != 'N':
-    codigo = int(input('Digite o cod. do Aluno para consulta: '))
-    print(f'{"Cod.":<5} {"NOME":<10} {"1ª NOTA":>10} {"2ª NOTA":>10}')
-    if codigo >= 1 and codigo <= len(lista):
-        for indice, aluno in enumerate(lista):
-            if (codigo - 1) == indice:
-                print('-' *38)
-                print(f'{indice + 1:<5} {aluno[0]:<10} {aluno[1]:>10.1f} {aluno[2]:>10.1f}')
-                print('-' *38)
-            else:
-                pass
-    else:
-        print('Codigo digitado inexistente!')
-    print()
-    opcao = str(input('Deseja consultar outro aluno? [S/N] ')).strip().upper()
-print('-' * 32)
-print('-------Sistema Finalizado!------')
-print('-' * 32)
+#Folha de Pagamento
+salario_bruto = float(input(f'Digite o valor do salário para o calculo da folha: R$'))
+if salario_bruto >= 1100:
+    #Calculo do valor do IR
+    def calculo_ir():
+        if salario_bruto <= 1903.98:
+            valor_ir = salario_bruto * 0 / 100
+        elif salario_bruto <= 2826.65:
+            valor_ir = salario_bruto * 7.5 / 100
+        elif salario_bruto <= 3751.05:
+            valor_ir = salario_bruto * 15 / 100
+        else:
+            valor_ir = salario_bruto * 27.5 / 100
+        return valor_ir
+
+
+    #Calculo do valor do INSS
+    def calculo_inss():
+        if salario_bruto <= 1100:
+            valor_inss = salario_bruto * 7.5 / 100
+        elif salario_bruto <= 2203.49:
+            valor_inss = salario_bruto * 9 / 100
+        elif salario_bruto <= 3305.22:
+            valor_inss = salario_bruto * 12 / 100
+        else:
+            valor_inss = salario_bruto * 14 / 100
+        return valor_inss
+
+
+    ir = calculo_ir()
+    inss = calculo_inss()
+    #Calculo do valor do Sindicato
+    sindicato = salario_bruto * 3 / 100
+    #Calculo do Salario Liquido
+    salario_liquido = salario_bruto - (ir + inss + sindicato)
+    
+    
+    #Impressão Demonstrativo
+    print(f'''
+-------Demonstrativo-------
+
+Salario Bruto:    R${salario_bruto:.2f}
+Desconto IR:      R${ir:.2f}
+Desconto INSS:    R${inss:.2f}
+Contrib. Sind:    R${sindicato:.2f}
+Salario Liquido:  R${salario_liquido:.2f}
+''')
+else:
+    print('O salário digitado nao pode ser menor que o salário mínimo atual!')
