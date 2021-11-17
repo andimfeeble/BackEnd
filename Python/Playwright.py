8cf471ba48143fb72d7cef8dc8252950e8805c55
andimfeeble <andimfeeble@hotmail.com>
2021-11-17 12:15:07 -0300

    Excluisão dos arquivos Selenium e Playwright

--- Python/Playwright.py
+++
@@ -1,7 +0,0 @@
-from playwright.sync_api import sync_playwright
-with sync_playwright() as p:
-    browser = p.webkit.launch(headless=False)
-    page = browser.new_page()
-    page.goto("http://playwright.dev")
-    print(page.title())
-    browser.close()
